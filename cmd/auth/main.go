0b26f3f0c408166623d9ca99db80e90102d93846
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-21 14:46:39 +0300

    feat: add configuration

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "log"
+import (
+	"github.com/AleksK1NG/auth-microservice/config"
+	"github.com/AleksK1NG/auth-microservice/pkg/utils"
+	"log"
+	"os"
+)
 
 func main() {
 	log.Println("Starting auth microservice")
+
+	configPath := utils.GetConfigPath(os.Getenv("config"))
+
+	cfgFile, err := config.LoadConfig(configPath)
+	if err != nil {
+		log.Fatalf("LoadConfig: %v", err)
+	}
+
+	cfg, err := config.ParseConfig(cfgFile)
+	if err != nil {
+		log.Fatalf("ParseConfig: %v", err)
+	}
+
+	log.Printf("Success parsed config: %#v", cfg.Server.AppVersion)
 }
---
+++ pkg/utils/config.go
@@ -0,0 +1,9 @@
+package utils
+
+// Get config path for local or docker
+func GetConfigPath(configPath string) string {
+	if configPath == "docker" {
+		return "./config/config-docker"
+	}
+	return "./config/config-local"
+}
