d0aa5681ab0fa85edd73885efc68013bb47813ad
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-22 12:00:10 +0300

    feat: add user proto update and migration init

---
+++ migrations/01_create_initial_tables.up.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS CITEXT;
+-- CREATE EXTENSION IF NOT EXISTS postgis;
+-- CREATE EXTENSION IF NOT EXISTS postgis_topology;
+
+
+CREATE TYPE role AS ENUM ('admin', 'user');
+
+CREATE TABLE users
+(
+    user_id    UUID PRIMARY KEY                  DEFAULT uuid_generate_v4(),
+    first_name VARCHAR(32)              NOT NULL CHECK ( first_name <> '' ),
+    last_name  VARCHAR(32)              NOT NULL CHECK ( last_name <> '' ),
+    email      VARCHAR(64) UNIQUE       NOT NULL CHECK ( email <> '' ),
+    password   VARCHAR(250)             NOT NULL CHECK ( octet_length(password) <> 0 ),
+    role       role                     NOT NULL DEFAULT 'user',
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE          DEFAULT CURRENT_TIMESTAMP
+);
