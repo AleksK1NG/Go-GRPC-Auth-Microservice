e0263faf08cbf313535eef10f912b5dfbc33314c
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-23 16:15:37 +0300

    feat: add metrics fix

--- internal/user/delivery/grpc/service/service.go
+++ internal/user/delivery/grpc/service/service.go
@@ -5,16 +5,18 @@ import (
 	"github.com/AleksK1NG/auth-microservice/internal/session"
 	"github.com/AleksK1NG/auth-microservice/internal/user"
 	"github.com/AleksK1NG/auth-microservice/pkg/logger"
+	"github.com/AleksK1NG/auth-microservice/pkg/metric"
 )
 
 type usersService struct {
-	logger logger.Logger
-	cfg    *config.Config
-	userUC user.UserUseCase
-	sessUC session.SessionUseCase
+	logger  logger.Logger
+	cfg     *config.Config
+	userUC  user.UserUseCase
+	sessUC  session.SessionUseCase
+	metrics metric.Metrics
 }
 
 // Auth service constructor
-func NewAuthServerGRPC(logger logger.Logger, cfg *config.Config, userUC user.UserUseCase, sessUC session.SessionUseCase) *usersService {
-	return &usersService{logger: logger, cfg: cfg, userUC: userUC, sessUC: sessUC}
+func NewAuthServerGRPC(logger logger.Logger, cfg *config.Config, userUC user.UserUseCase, sessUC session.SessionUseCase, metrics metric.Metrics) *usersService {
+	return &usersService{logger: logger, cfg: cfg, userUC: userUC, sessUC: sessUC, metrics: metrics}
 }
